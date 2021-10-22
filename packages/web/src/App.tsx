ca2f98882d4c0c38e80cc730e9fe94a91a6e2be3
Rebeca Lopes <becatrizlopes@gmail.com>
2021-10-22 11:13:46 -0400

    fix(wbe): altera web

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 // import api from '@monorepo/axios-config'
 
 const App: React.FC = () => {
-  return <h1>Hello Worldddddd</h1>
+  return <h1>Hello World</h1>
 }
 
 export default App
